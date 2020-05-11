b4669e5476943ccfc0d03d369470c624070ce39e
Tyler Blakeman <tylerblakeman@gmail.com>
2020-05-11 18:40:38 -0500

    updating with content I have so far

---
+++ index.js
@@ -0,0 +1,20 @@
+var letter = require("./Letter.js");
+var word = require("./Word.js");
+var inquirer = require("inquirer")
+
+var words = [
+    "Vincent van Gogh",
+    "Claude Monet",
+    "Pablo Picasso",
+    "Leonardo da Vinci",
+    "Rembrandt",
+    "Michelangelo",
+    "Salvador Dali",
+    "Gustav Klimt",
+    "Pierre-Auguste Renoir",
+    "Johannes Vermeer",
+    "Edgar Degas",
+    "Edvard Munch",
+    "Frida Kahlo",
+    "Jackson Pollock"
+]
